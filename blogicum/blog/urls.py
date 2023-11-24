fb7d9f97ad7566f3a22d43df6d7d36322981476e
Амина Юнисова <amina.yunisova@gmail.com>
2023-11-24 14:45:52 +0100

    14:45 24/11/2023

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('posts/<int:id>/', post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', category_posts,
          name='category_posts'),
-    path('post/create/', create_post, name='create_post'),
+    path('posts/create/', create_post, name='create_post'),
     path('posts/<int:post_id>/edit/',
          EditPostView.as_view(),
          name='edit_post'),
